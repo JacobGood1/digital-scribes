d40dc747b895973b4db7ed3854914bfca446823b
Travis OBrien <travis.akablyd@hotmail.com>
2017-01-30 21:57:13 -0500

    Removed Test Mask from Advert
    Code for mask is just commented, for future references.

--- web/kickstarter_advert.dart
+++ web/kickstarter_advert.dart
@@ -4,7 +4,7 @@ class KickstarterAdvert extends Group {
   num height = 1080;
   //resourceManager.getBitmapData('button_up')
   KickstarterAdvert(ResourceManager resourceManager){
-    num triangleWidth = 200.0;
+    /*num triangleWidth = 200.0;
     num triangleHeight = 50.0;
     List<Point> points = new List<Point>();
     points.add(new Point(0, 0));
@@ -14,12 +14,13 @@ class KickstarterAdvert extends Group {
     points.add(new Point(1920/2, 1080 - triangleHeight));
     points.add(new Point(1920/2-triangleWidth/2, 1080));
     points.add(new Point(0, 1080));
+    Mask mask = new Mask.custom(points);
+    advert.mask = mask;*/
 
     renderOrder = 1;
-    Mask mask = new Mask.custom(points);
     Bitmap advert = new Bitmap(resourceManager.getBitmapData('kickstarter_advert'));
     addChild(advert);
-    advert.mask = mask;
+
     super.setupPosition();
   }
 }
