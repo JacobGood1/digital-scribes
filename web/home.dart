621e4c57105055fa71961213d80d12614cc2c7a1
Travis OBrien <travis.akablyd@hotmail.com>
2017-02-07 20:43:38 -0500

    maximizing asset load times WIP

--- web/home.dart
+++ web/home.dart
@@ -6,22 +6,23 @@ import 'navigation.dart';
 import 'recent_news.dart';
 
 class Home extends Page{
-
   Home () {
     var time = new DateTime.now().millisecondsSinceEpoch;
     resourceManager = new ResourceManager()
       ..addBitmapData('kickstarter_advert', 'resources/udemy_kickstarter_ue4.png')
       ..addBitmapData('plan', 'resources/planOfAction.png')
       ..addBitmapData('timmysNightmare_Title', 'resources/title_timmysNightmare.png')
+      ..addVideo('digitalScribes_logo', 'resources/digitalScribes_logo.mp4')
+      ..addBitmapData('digitalScribes_logo_mask', 'resources/digitalScribes_logo_mask.png')
       ..addVideo('timmy_example', 'resources/timmy_example.mp4')
-      ..addTextureAtlas('logo_digitalScribes_atlas', 'resources/digitalScribes_logo_atlas/digitalScribes_logo_atlas.atlas', TextureAtlasFormat.LIBGDX)
+      //..addTextureAtlas('logo_digitalScribes_atlas', 'resources/digitalScribes_logo_atlas/digitalScribes_logo_atlas.atlas', TextureAtlasFormat.LIBGDX)
       ..addBitmapData('button_news', 'resources/icon_news.svg')
       ..addBitmapData('button_tutorials', 'resources/icon_tutorials.svg')
       ..addBitmapData('button_contactUs', 'resources/icon_contactUs.svg')
     ;
 
     resourceManager.load().then((result){
-
+      print("TIME: ${new DateTime.now().millisecondsSinceEpoch - time}");
       //instantiate all elements - create an abstract element tree
       new KickstarterAdvert(this);
       new DigitalScribesLogo(this);
@@ -31,7 +32,6 @@ class Home extends Page{
       //an abstract element tree has been created, use that information and add all elements to the stage.
       addToStage();
       finalizeCanvasDimensions();
-      print("TIME: ${new DateTime.now().millisecondsSinceEpoch - time}");
     });
   }
 }
