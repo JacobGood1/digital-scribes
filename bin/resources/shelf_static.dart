b93faf9c1cf70dd22c6b29d054e2b4eb338cdcfe
Jacob Goodson <jacobegoodson@gmail.com>
2017-05-18 16:20:03 -0400

    trying to get post to work

--- bin/resources/shelf_static.dart
+++ bin/resources/shelf_static.dart
@@ -1,7 +1,16 @@
 part of server;
 
-@Route("/")
+
+
+@Route("/", methods: const [GET, POST])
 void shelfStatic(HttpRequest request) {
   io.handleRequest(request, createStaticHandler(cmdArgs['static'], defaultDocument: 'index.html'));
+}
+
+@Route("/api/stuff", method: POST)
+void input_email(HttpRequest request) {
+  //io.handleRequest(request, createStaticHandler(cmdArgs['static'], defaultDocument: 'index.html'));
+  print("posted!");
+
 }
 
--- bin/server.dart
+++ bin/server.dart
@@ -6,6 +6,7 @@ import 'dart:async';
 
 // Import from pub.dartlang.org
 import 'package:vane/vane_server.dart';
+import 'package:shelf_route/shelf_route.dart' as sh;
 import 'package:shelf/shelf.dart' as shelf;
 import 'package:shelf/shelf_io.dart' as io;
 import 'package:shelf_static/shelf_static.dart';
@@ -35,6 +36,6 @@ void main(List<String> args) {
 
   // NOTE: If the environment variable "PORT" is set, that overrides 7777 below. When deploying to Sourcevoid for
   // example the PORT variable is used so locally you can use whatever port you like.
-  serve(address: '0.0.0.0', port: 7777);
+  serve(address: '0.0.0.0', port: 8080);
 }
 
