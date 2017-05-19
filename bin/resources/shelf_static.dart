2bb0642550b7b703001ba1df52ea439ff51fe68d
Jacob Goodson <jacobegoodson@gmail.com>
2017-05-18 22:47:06 -0400

    still not working

--- bin/resources/shelf_static.dart
+++ bin/resources/shelf_static.dart
@@ -1,16 +1,19 @@
 part of server;
 
 
+class Handlers extends Vane {
 
-@Route("/", methods: const [GET, POST])
-void shelfStatic(HttpRequest request) {
-  io.handleRequest(request, createStaticHandler(cmdArgs['static'], defaultDocument: 'index.html'));
-}
+  @Route("/")
+  Future shelfStatic(HttpRequest request) {
+    io.handleRequest(request, createStaticHandler(cmdArgs['static'], defaultDocument: 'index.html'));
+    return close();
+  }
 
-@Route("/api/stuff", method: POST)
-void input_email(HttpRequest request) {
-  //io.handleRequest(request, createStaticHandler(cmdArgs['static'], defaultDocument: 'index.html'));
-  print("posted!");
-
+  @Route("/api/stuff", method: POST)
+  Future input_email() {
+    writeln('Received request ${req.method}: ${req.uri.path}');
+    writeln('Data: ${params}');
+    return close();
+  }
 }
 
