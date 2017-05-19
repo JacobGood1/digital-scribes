9ce43f5576f930444ff36d9deb920040087ac69d
Jacob Goodson <jacobegoodson@gmail.com>
2017-05-19 12:49:38 -0400

    copied example

--- bin/resources/shelf_static.dart
+++ bin/resources/shelf_static.dart
@@ -1,14 +1,12 @@
 part of server;
 
 
+@Route("/")
+Future shelfStatic(HttpRequest request) {
+  io.handleRequest(request, createStaticHandler(cmdArgs['static'], defaultDocument: 'index.html'));
+}
+
 class Handlers extends Vane {
-
-  @Route("/")
-  Future shelfStatic(HttpRequest request) {
-    io.handleRequest(request, createStaticHandler(cmdArgs['static'], defaultDocument: 'index.html'));
-    return close();
-  }
-
   @Route("/api/stuff", method: POST)
   Future input_email() {
     writeln('Received request ${req.method}: ${req.uri.path}');
